d32bee1951e7e27c7519c4dd7730a3edd27c6756
mallu-harshavardhan07 <malluharshavardhanreddy0705@gmail.com>
2025-10-26 15:59:35 +0530

    done

--- vpc-module/output.tf
+++ vpc-module/output.tf
@@ -2,13 +2,13 @@ output "vpc_id" {
   value = aws_vpc.main.id
 }
 
-output "public_subnet_ids" {
+output "public_subnet" {
   value = aws_subnet.public_subnet[*].id
 }
-output "private_subnet_ids" {
+output "private_subnet" {
   value = aws_subnet.private[*].id
 }
 
-output "database_subnet_ids" {
+output "database_subnet" {
   value = aws_subnet.database[*].id
 }
